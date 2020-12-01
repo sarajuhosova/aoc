98ce7b78c8251964cfef68562db257a871859546
Sára Juhošová <s.juhosova@student.tudelft.nl>
2020-12-01 07:36:33 +0100

    Configure Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 plugins {
     java
+
+    // Spotless plugin for checking style of Java code.
+    id("com.diffplug.gradle.spotless").version("3.27.2")
 }
 
 group = "org.example"
@@ -13,4 +18,21 @@ dependencies {
 	testImplementation("org.junit.jupiter", "junit-jupiter", "5.6.2")
     runtimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.3.1")
     
+}
+
+// Configure Spotless plugin for style checking Java code.
+configure<SpotlessExtension> {
+    java {
+        // Use the eclipse formatter format and import order.
+        eclipse().configFile(file("eclipse-formatter.xml"))
+        importOrderFile(file("$rootDir/importorder.txt"))
+
+        targetExclude("build/")
+
+        // Default added rules.
+        paddedCell()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
