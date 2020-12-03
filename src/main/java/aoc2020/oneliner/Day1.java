715dfaf5b0aaab1fcdbe75e0c29f354f5c38d15e
Sára Juhošová <s.juhosova@student.tudelft.nl>
2020-12-03 10:27:29 +0100

    Do 2020-D3

---
+++ src/main/java/aoc2020/oneliner/Day1.java
@@ -0,0 +1,37 @@
+package aoc2020.oneliner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Day1 {
+
+	public static int part1() throws IOException {
+		return ((Function<List<Integer>, Integer>) s -> s.stream()
+				.filter(i -> s.stream().anyMatch(j -> j == 2020 - i))
+				.map(i -> i * (2020 - i))
+				.findAny().get()
+		).apply(Files.lines(Path.of("src/main/resources/aoc2020/day1.txt"))
+				.map(Integer::parseInt).collect(Collectors.toList()));
+	}
+
+	public static int part2() throws IOException {
+		return ((Function<List<Integer>, Integer>) s -> s.stream()
+				.flatMap(a -> s.stream()
+						.flatMap(b -> s.stream()
+								.filter(c -> a + b + c == 2020)
+								.map(c -> a * b * c)))
+				.findAny().get()
+		).apply(Files.lines(Path.of("src/main/resources/aoc2020/day1.txt"))
+				.map(Integer::parseInt).collect(Collectors.toList()));
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(part1());
+		System.out.println(part2());
+	}
+
+}
---
+++ src/main/java/aoc2020/oneliner/Day2.java
@@ -0,0 +1,41 @@
+package aoc2020.oneliner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import library.tuple.Quadruple;
+import library.tuple.Triple;
+
+public class Day2 {
+
+	public static long part1() throws IOException {
+		return Files.lines(Path.of("src/main/resources/aoc2020/day2.txt"))
+				.map(l -> l.split(" |-"))
+				.map(a -> new Triple<Integer, Integer, Long>(
+						Integer.parseInt(a[0]),
+						Integer.parseInt(a[1]),
+						a[3].chars().filter(c -> c == (int) a[2].charAt(0)).count()))
+				.filter(t -> t.getThird() >= t.getFirst() && t.getThird() <= t.getSecond())
+				.count();
+	}
+
+	public static long part2() throws IOException {
+		return Files.lines(Path.of("src/main/resources/aoc2020/day2.txt"))
+				.map(l -> l.split(" |-"))
+				.map(a -> new Quadruple<Integer, Integer, Character, String>(
+						Integer.parseInt(a[0]),
+						Integer.parseInt(a[1]),
+						a[2].charAt(0),
+						a[3]))
+				.filter(q -> (q.getFourth().charAt(q.getFirst() - 1) == q.getThird())
+						^ (q.getFourth().charAt(q.getSecond() - 1) == q.getThird()))
+				.count();
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(part1());
+		System.out.println(part2());
+	}
+
+}
