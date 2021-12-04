2c3b8dcdc55a0df5ff0c7355b67ef4e0d5c57a03
Sára Juhošová <s.juhosova@student.tudelft.nl>
2021-12-04 08:49:43 +0100

    Do 2021 D4

---
+++ src/main/java/aoc2021/day04/Bingo.java
@@ -0,0 +1,71 @@
+package aoc2021.day04;
+
+import library.Year;
+import library.io.Input;
+import library.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Bingo {
+
+    private static Pair<List<Board>, List<Integer>> read() {
+        Scanner sc = Input.openFile(Year.AOC_2021, "day4.txt");
+
+        List<Integer> draws = Arrays.stream(sc.nextLine().split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Board> boards = new ArrayList<>();
+        while (sc.hasNext()) {
+            sc.nextLine();
+            boards.add(Board.read(sc));
+        }
+
+        return new Pair<>(boards, draws);
+    }
+
+    private static Pair<Board, Integer> winner(List<Board> boards, List<Integer> draws) {
+        for (Integer num : draws) {
+            for (Board board : boards) {
+                board.setNumbers(num);
+                if (board.won()) {
+                    return new Pair<>(board, num);
+                }
+            }
+        }
+        return null;
+    }
+
+    private static Pair<Board, Integer> loser(List<Board> boards, List<Integer> draws) {
+        Pair<Board, Integer> winner = winner(boards, draws);
+
+        boards.remove(winner.getFirst());
+
+        if (boards.size() == 1) {
+            return winner(boards, draws);
+        }
+        return loser(boards, draws);
+    }
+
+    private static void printPair(Pair<Board, Integer> pair) {
+        System.out.println(pair.getFirst().getScore() * pair.getSecond());
+    }
+
+    public static void main(String[] args) {
+        // load data
+        Pair<List<Board>, List<Integer>> data = read();
+        List<Board> boards = data.getFirst();
+        List<Integer> draws = data.getSecond();
+
+        // part 1
+        printPair(winner(boards, draws));
+
+        // part 2
+        printPair(loser(boards, draws));
+    }
+
+}
---
+++ src/main/java/aoc2021/day04/Board.java
@@ -0,0 +1,84 @@
+package aoc2021.day04;
+
+import java.util.Scanner;
+
+public class Board {
+
+    private int[][] numbers;
+    private boolean[][] marked;
+
+    public Board(int[][] numbers) {
+        this.numbers = numbers;
+        this.marked = new boolean[numbers.length][numbers[0].length];
+    }
+
+    public void setNumbers(int number) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[0].length; j++) {
+                if (number == numbers[i][j])
+                    marked[i][j] = true;
+            }
+        }
+    }
+
+    private boolean hasHorizontal() {
+        for (int i = 0; i < numbers.length; i++) {
+            int count = 0;
+            for (int j = 0; j < numbers[i].length; j++) {
+                if (marked[i][j]) count++;
+            }
+            if (count == numbers[i].length)
+                return true;
+        }
+        return false;
+    }
+
+    private boolean hasVertical() {
+        for (int j = 0; j < numbers[0].length; j++) {
+            int count = 0;
+            for (int i = 0; i < numbers[j].length; i++) {
+                if (marked[i][j]) count++;
+            }
+            if (count == numbers[j].length)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean won() {
+        return hasHorizontal() || hasVertical();
+    }
+
+    public int getScore() {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[0].length; j++) {
+                if (!marked[i][j]) sum += numbers[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static Board read(Scanner sc) {
+        int[][] board = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        return new Board(board);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int[] number : numbers) {
+            for (int num : number) {
+                builder.append(String.format("%2d ", num));
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+}
