131a2e4d8d0ddc13fe009149c1b74a58a578b425
Sára Juhošová <s.juhosova@student.tudelft.nl>
2021-12-09 13:37:47 +0100

    Do 2021 Day 9

---
+++ src/main/java/aoc2021/day09/FindBasins.java
@@ -0,0 +1,101 @@
+package aoc2021.day09;
+
+import library.Year;
+import library.io.Input;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FindBasins {
+
+    private static int[][] convertToArray(List<List<Integer>> read) {
+        int[][] data = new int[read.size()][read.get(0).size()];
+        for (int i = 0; i < read.size(); i++) {
+            for (int j = 0; j < read.get(0).size(); j++) {
+                data[i][j] = read.get(i).get(j);
+            }
+        }
+        return data;
+    }
+
+    private static int[][] read() {
+        Scanner sc = Input.openFile(Year.AOC_2021, "day09.txt");
+        List<List<Integer>> read = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            read.add(sc.nextLine().chars()
+                    .mapToObj(c -> c - '0')
+                    .collect(Collectors.toList()));
+        }
+        return convertToArray(read);
+    }
+
+    private static boolean isLocalMinimum(int[][] data, int i, int j) {
+        int here = data[i][j];
+        if (i > 0 && data[i - 1][j] <= here) return false;
+        if (j > 0 && data[i][j - 1] <= here) return false;
+        if (i < data.length - 1 && data[i + 1][j] <= here) return false;
+        if (j < data[0].length - 1 && data[i][j + 1] <= here) return false;
+        return true;
+    }
+
+    private static List<Point> getMinima(int[][] data) {
+        List<Point> minima = new ArrayList<>();
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                if (isLocalMinimum(data, i, j)) {
+                    minima.add(new Point(i, j));
+                }
+            }
+        }
+        return minima;
+    }
+
+    private static long getRiskLevel(int[][] data, List<Point> minima) {
+        long sum = 0;
+        for (Point loc : minima) {
+            sum += 1 + data[loc.getX()][loc.getY()];
+        }
+        return sum;
+    }
+
+    private static void setBasinLocations(int[][] data, Set<Point> points, Point point) {
+        int i = point.getX();
+        int j = point.getY();
+        if (points.contains(point) || data[i][j] == 9) return;
+        points.add(point);
+        if (i > 0) setBasinLocations(data, points, new Point(i - 1, j));
+        if (j > 0) setBasinLocations(data, points, new Point(i, j - 1));
+        if (i < data.length - 1) setBasinLocations(data, points, new Point(i + 1, j));
+        if (j < data[0].length - 1) setBasinLocations(data, points, new Point(i, j + 1));
+    }
+
+    private static int getBasinSize(int[][] data, Point minimum) {
+        Set<Point> points = new HashSet<>();
+        setBasinLocations(data, points, minimum);
+        return points.size();
+    }
+
+    private static int findLargestBasins(int[][] data, List<Point> minima) {
+        List<Integer> basins = minima.stream()
+                .map(m -> getBasinSize(data, m))
+                .sorted()
+                .collect(Collectors.toList());
+
+        return basins.get(basins.size() - 1)
+                * basins.get(basins.size() - 2)
+                * basins.get(basins.size() - 3);
+    }
+
+    public static void main(String[] args) {
+        // load data
+        int[][] data = read();
+        List<Point> minima = getMinima(data);
+
+        // Part 1
+        System.out.println(getRiskLevel(data, minima));
+
+        // Part 2
+        System.out.println(findLargestBasins(data, minima));
+    }
+
+}
---
+++ src/main/java/aoc2021/day09/Point.java
@@ -0,0 +1,44 @@
+package aoc2021.day09;
+
+class Point {
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (x != point.x) return false;
+        return y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+}
