4f4a43a9734ccfc3fb7d299ec52ca6424fd25b35
Sára Juhošová <s.juhosova@student.tudelft.nl>
2023-12-01 06:45:46 +0100

    Do 2023-D1

--- kotlin/src/main/kotlin/aoc2023/day01/Day01.kt
+++ kotlin/src/main/kotlin/aoc2023/day01/Day01.kt
@@ -3,12 +3,53 @@ package aoc2023.day01
 import library.Year
 import library.readData
 
-fun part1(data: List<String>): Int {
-    return 0;
+val WORD_TO_NUM = mapOf(
+    Pair("one", 1),
+    Pair("two", 2),
+    Pair("three", 3),
+    Pair("four", 4),
+    Pair("five", 5),
+    Pair("six", 6),
+    Pair("seven", 7),
+    Pair("eight", 8),
+    Pair("nine", 9)
+)
+
+fun searchForDigits(data: List<String>): Int =
+    data.map {
+        it.filter { c -> c in '0'..'9' }
+    }.sumOf { s -> (s[0] - '0') * 10 + (s[s.length - 1] - '0') }
+
+fun checkForDigit(s: String): Int? {
+    // check whether the first number is an actual digit
+    if (s[0] in '0'..'9') return (s[0] - '0')
+    // otherwise check for a word
+    val key = WORD_TO_NUM.keys.find { s.startsWith(it) }
+    return if (key != null) WORD_TO_NUM[key]!! else null
 }
 
-fun part2(data: List<String>): Int {
-    return 0;
+fun searchForAllDigits(data: List<String>): Int {
+    var sum = 0
+
+    for (line in data) {
+        for (i in line.indices) {
+            val maybe = checkForDigit(line.substring(i))
+            if (maybe != null) {
+                sum += maybe * 10
+                break
+            }
+        }
+
+        for (i in (line.length - 1) downTo 0) {
+            val maybe = checkForDigit(line.substring(i))
+            if (maybe != null) {
+                sum += maybe
+                break
+            }
+        }
+    }
+
+    return sum
 }
 
 fun main() {
@@ -16,6 +57,6 @@ fun main() {
 
     val data = readData(Year._2023, 1)
 
-    println(part1(data))
-    println(part2(data))
+    println(searchForDigits(data))
+    println(searchForAllDigits(data))
 }
