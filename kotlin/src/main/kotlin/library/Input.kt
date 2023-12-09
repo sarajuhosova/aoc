a058f0b50824ba98d5ea4a7cb870c755e17ba2d4
Sára Juhošová <s.juhosova@student.tudelft.nl>
2023-12-09 12:08:04 +0100

    Refactor reading data

--- kotlin/src/main/kotlin/library/Input.kt
+++ kotlin/src/main/kotlin/library/Input.kt
@@ -4,16 +4,19 @@ import java.io.BufferedReader
 import java.io.File
 import java.security.InvalidParameterException
 
+private const val FILENAME = "day%02d/%s.txt"
+private const val DEFAULT_FILE = "in"
+
 
 fun getReader(year: Year, filename: String): BufferedReader =
     File("src/main/resources/" + year.directory + "/" + filename)
         .bufferedReader()
 
-fun getReader(year: Year, day: Int, trail: String = ""): BufferedReader {
+fun getReader(year: Year, day: Int, trail: String = DEFAULT_FILE): BufferedReader {
     if (day < 1 || day > 25) {
         throw InvalidParameterException("Day must be between 1 and 25")
     }
-    return getReader(year, String.format("day%02d%s.txt", day, trail))
+    return getReader(year, String.format(FILENAME, day, trail))
 }
 
 fun readFirst(year: Year, filename: String): String =
@@ -26,11 +29,11 @@ fun readData(year: Year, filename: String): List<String> =
         .bufferedReader()
         .readLines()
 
-fun readData(year: Year, day: Int, trail: String = ""): List<String> {
+fun readData(year: Year, day: Int, name: String = DEFAULT_FILE): List<String> {
     if (day < 1 || day > 25) {
         throw InvalidParameterException("Day must be between 1 and 25")
     }
-    return readData(year, String.format("day%02d%s.txt", day, trail))
+    return readData(year, String.format(FILENAME, day, name))
 }
 
 fun readInts(year: Year, filename: String) : List<Int> =
