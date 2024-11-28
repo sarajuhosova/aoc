65de799e87383dc191bc1ae2fee935a7196d17a2
Sára Juhošová <sarajuhosova@gmail.com>
2024-11-28 10:57:32 +0100

    Attempt IntCode D13P2

--- kotlin/src/main/kotlin/aoc2019/Day13.kt
+++ kotlin/src/main/kotlin/aoc2019/Day13.kt
@@ -6,13 +6,14 @@ import kotlinx.coroutines.runBlocking
 import library.Coordinate
 import library.Year
 import library.readFirst
+import kotlin.random.Random
 
-enum class Tile {
-    EMPTY,  // 0 is an empty tile. No game object appears in this tile.
-    WALL,   // 1 is a wall tile. Walls are indestructible barriers.
-    BLOCK,  // 2 is a block tile. Blocks can be broken by the ball.
-    PADDLE, // 3 is a horizontal paddle tile. The paddle is indestructible.
-    BALL;    // 4 is a ball tile. The ball moves diagonally and bounces off objects.
+enum class Tile(val char: Char) {
+    EMPTY(' '),  // 0 is an empty tile. No game object appears in this tile.
+    WALL('|'),   // 1 is a wall tile. Walls are indestructible barriers.
+    BLOCK('□'),  // 2 is a block tile. Blocks can be broken by the ball.
+    PADDLE('_'), // 3 is a horizontal paddle tile. The paddle is indestructible.
+    BALL('•');    // 4 is a ball tile. The ball moves diagonally and bounces off objects.
 
     companion object {
         private val map = entries.associateBy { it.ordinal }
@@ -20,12 +21,32 @@ enum class Tile {
     }
 }
 
-class ArcadeGame : IO() {
-    private val tiles = mutableMapOf<Coordinate, Tile>()
-    fun getTiles(): Map<Coordinate, Tile> = tiles
+class ArcadeGame {
+    private val blocks = mutableSetOf<Coordinate>()
+    private val walls = mutableSetOf<Coordinate>()
+    fun blocks(): Set<Coordinate> = blocks
 
+    private var paddle: Coordinate? = null
+    private var ball: Coordinate? = null
+
+    private var score = 0L
+    fun score() = score
+    fun updateScore(s: Long) { score = s }
+
+    fun position(coordinate: Coordinate, tile: Tile) {
+        when (tile) {
+            Tile.EMPTY -> {} // do nothing
+            Tile.WALL -> walls.add(coordinate)
+            Tile.BLOCK -> blocks.add(coordinate)
+            Tile.PADDLE -> paddle = coordinate
+            Tile.BALL -> ball = coordinate
+        }
+    }
+}
+
+class ArcadeGameInterface(private val game: ArcadeGame) : IO() {
     enum class Input {
-        X, Y, ID;
+        X, Y, ID_OR_SCORE;
 
         fun next() = entries[(this.ordinal + 1) % entries.size]
     }
@@ -33,15 +54,19 @@ class ArcadeGame : IO() {
     private var x: Int? = null
     private var y: Int? = null
 
-    override suspend fun provide(): Long {
-        TODO("Not yet implemented")
-    }
+    override suspend fun provide(): Long = Random.nextLong(-1, 2)
 
     override suspend fun write(out: Long) {
         when (expected) {
             Input.X -> x = out.toInt()
             Input.Y -> y = out.toInt()
-            Input.ID -> tiles[Coordinate(x!!, y!!)] = Tile.getTile(out.toInt())
+            Input.ID_OR_SCORE -> {
+                if (x == -1 && y == 0) {
+                    game.updateScore(out)
+                } else {
+                    game.position(Coordinate(x!!, y!!), Tile.getTile(out.toInt()))
+                }
+            }
         }
         expected = expected.next()
     }
@@ -51,6 +76,10 @@ fun main() {
     val computer = Computer(readFirst(Year._2019, 13))
 
     val game1 = ArcadeGame()
-    runBlocking { computer.run(io = game1) }
-    println(game1.getTiles().values.count { it == Tile.BLOCK })
+    runBlocking { computer.run(io = ArcadeGameInterface(game1)) }
+    println(game1.blocks().size)
+
+    val game2 = ArcadeGame()
+    runBlocking { computer.run(mapOf(0 to 2L), io = ArcadeGameInterface(game2)) }
+    println(game2.score())
 }
--- kotlin/src/test/kotlin/aoc2019/intcode/Day13.kt
+++ kotlin/src/test/kotlin/aoc2019/intcode/Day13.kt
@@ -1,7 +1,7 @@
 package aoc2019.intcode
 
+import aoc2019.ArcadeGameInterface
 import aoc2019.ArcadeGame
-import aoc2019.Tile
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
@@ -11,8 +11,8 @@ class Day13: ComputerTest(13) {
     @Test
     fun part1Test() {
         val game = ArcadeGame()
-        runBlocking { computer().run(io = game) }
-        assertThat(game.getTiles().values.count { it == Tile.BLOCK }).isEqualTo(312)
+        runBlocking { computer().run(io = ArcadeGameInterface(game)) }
+        assertThat(game.blocks()).hasSize(312)
     }
 
 }
