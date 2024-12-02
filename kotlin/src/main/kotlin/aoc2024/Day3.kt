c9aee93578312dfc7cfa9c8e801a0cad3ec8d849
Sára Juhošová <sarajuhosova@gmail.com>
2024-12-02 12:46:22 +0100

    Prepare for Day 3

---
+++ kotlin/src/main/kotlin/aoc2024/Day3.kt
@@ -0,0 +1,19 @@
+package aoc2024
+
+import library.Year
+import library.readData
+
+fun part1(data: List<String>): Int {
+    return 0
+}
+
+fun part2(data: List<String>): Int {
+    return 0
+}
+
+fun main() {
+    val data = readData(Year._2024, 3)
+
+    println(part1(data))
+    println(part2(data))
+}
