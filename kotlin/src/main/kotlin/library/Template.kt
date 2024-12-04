274ed873fd27ed5da156025dff2b10ceeb3ad814
Sára Juhošová <sarajuhosova@gmail.com>
2024-12-04 09:43:41 +0100

    Update template

--- kotlin/src/main/kotlin/library/Template.kt
+++ kotlin/src/main/kotlin/library/Template.kt
@@ -1,16 +1,5 @@
 package library
 
-fun part1(data: List<String>): Int {
-    return 0
-}
-
-fun part2(data: List<String>): Int {
-    return 0
-}
-
 fun main() {
     val data = readData(Year._2024, 1)
-
-    println(part1(data))
-    println(part2(data))
 }
