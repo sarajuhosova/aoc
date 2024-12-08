3f90656965fc5dd3bc3fad4fccded30bbf0a425f
Sára Juhošová <sarajuhosova@gmail.com>
2024-12-08 10:03:19 +0100

    Update template

---
+++ kotlin/src/main/kotlin/aoc2024/Template.kt
@@ -0,0 +1,10 @@
+package aoc2024
+
+import library.Year
+import library.readData
+
+fun main() {
+    val data = readData(Year._2024, 1)
+
+    println(data.size)
+}
--- kotlin/src/main/kotlin/library/Template.kt
+++
@@ -1,5 +0,0 @@
-package library
-
-fun main() {
-    val data = readData(Year._2024, 1)
-}
