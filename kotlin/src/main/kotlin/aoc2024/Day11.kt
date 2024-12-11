a2a2880e036d4d72238d757c04d0eb11e50699a0
Sára Juhošová <sarajuhosova@gmail.com>
2024-12-11 13:42:57 +0100

    Do 2024 D11P2

--- kotlin/src/main/kotlin/aoc2024/Day11.kt
+++ kotlin/src/main/kotlin/aoc2024/Day11.kt
@@ -3,103 +3,46 @@ package aoc2024
 import library.Year
 import library.readFirst
 
-val ZEROS = mutableListOf<Long>()
-
-abstract class Stone {
-    abstract fun size(date: Int): Long
-}
-class ZeroStone(private val dob: Int) : Stone() {
-    override fun size(date: Int): Long = ZEROS[date - dob]
-}
-class NormalStone(private val engraving: Long): Stone() {
-    val string = engraving.toString()
-    
-    override fun size(date: Int): Long = 1L
-
-    fun multiply(): NormalStone = NormalStone(engraving * 2024)
-    
-    fun isEven(): Boolean = string.length % 2 == 0
-    fun split(dob: Int): Pair<Stone, Stone> = Pair(
-        string.substring(0, string.length / 2).toLong().toStone(dob),
-        string.substring(string.length / 2).toLong().toStone(dob)
+fun Long.split(): Pair<Long, Long> {
+    val string = this.toString()
+    return Pair(
+        string.substring(0, string.length / 2).toLong(),
+        string.substring(string.length / 2).toLong()
     )
 }
 
-fun Long.toStone(birth: Int = 0): Stone =
-    if (this == 0L) ZeroStone(birth) else NormalStone(this)
-
-fun blinkNaive(stones: MutableList<Long>) {
-    for (index in stones.lastIndex downTo 0) {
-        val engraving = stones[index]
-        if (engraving == 0L) stones[index] = 1
-        else {
-            val string = engraving.toString()
-            if (string.length % 2 == 0) {
-                stones[index] = string.substring(0, string.length / 2).toLong()
-                stones.add(index + 1, string.substring(string.length / 2).toLong())
-            }
-            else stones[index] = stones[index] * 2024
-        }
-    }
+fun MutableMap<Long, Long>.add(stone: Long, amount: Long) {
+    this[stone] = (this[stone] ?: 0L) + amount
 }
 
-fun blink(stones: MutableList<Stone>, dob: Int) {
-    for (index in stones.lastIndex downTo 0) {
-        val stone = stones[index]
-        if (stone is NormalStone) {
-            if (stone.isEven()) {
-                val (first, second) = stone.split(dob)
-                stones[index] = first
-                stones.add(index + 1, second)
-            } else {
-                stones[index] = stone.multiply()
-            }
+fun blink(stones: Map<Long, Long>): Map<Long, Long> {
+    val result = mutableMapOf<Long, Long>()
+    for ((engraving, amount) in stones) {
+        if (engraving == 0L) result.add(1, amount)
+        else {
+            if (engraving.toString().length % 2 == 0) {
+                val (left, right) = engraving.split()
+                result.add(left, amount)
+                result.add(right, amount)
+            } else result.add(engraving * 2024, amount)
         }
     }
+    return result
+}
+
+fun count(stones: Map<Long, Long>, repeat: Int): Long {
+    var mutable = stones
+    repeat (repeat) {
+        mutable = blink(mutable)
+    }
+    return mutable.map { (_, v) -> v }.sum()
 }
 
 fun main() {
     val stones = readFirst(Year._2024, 11)
         .split(" ").map { it.toLong() }
+        .associateWith { 1L }
 
-    // part1
-//    val mutable = stones.toMutableList()
-//    repeat (25) {
-//        blinkNaive(mutable)
-//    }
-//    println(mutable.size)
-
-    // part2
-    val blinks = 75
-
-//    val zeros = mutableListOf<Stone>(NormalStone(1))
-//    ZEROS.add(1)
-//    ZEROS.add(1)
-//    for (i in 1..blinks) {
-//        blink(zeros, i)
-//        ZEROS.add(zeros.sumOf { it.size(i) })
-//    }
-//    println(ZEROS.mapIndexed{ index, size -> "$index: $size" }.joinToString("\n"))
-
-    val efficient = stones.map { it.toStone() }.toMutableList()
-    for (i in 1..blinks) {
-        blink(efficient, i)
-    }
-    println(efficient.size)
-
-//    println(mutable.size)
+    println(count(stones, 25))
+    println(count(stones, 75))
 }
-
-/*
-0
-1
-2024
-20                                                     24
-2                               0                      2             4
-4048                            1                      4048          8096
-40            48                2024                   40            48                80              96
-4       0     4       8         20         24          4       0     4       8         8         0     9         6
-8096    1     8096    16192     2     0    2     4     8096    1     8096    16192     16192     1     18216     12144
-80  96  2024  80  96  32772608  4048  1    4048  8096  80  96  2024  80  96  32772608  32772608  2024  36869184  24579456
-8 0 9 6 20 24 8 0 9 6 3277 2608 40 48 2024 40 48 80 96 8 0 9 6 20 24 8 0 9 6 3277 2608 3277 2608 20 24 3686 9184 2457 9456
-*/
