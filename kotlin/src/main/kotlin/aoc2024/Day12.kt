33c89ef13d29b36808862634348ef602c9673d8f
Sára Juhošová <sarajuhosova@gmail.com>
2024-12-12 12:02:03 +0100

    Do 2024 D12

---
+++ kotlin/src/main/kotlin/aoc2024/Day12.kt
@@ -0,0 +1,48 @@
+package aoc2024
+
+import library.*
+
+fun Array<CharArray>.get(coordinate: Coordinate): Char =
+    this[coordinate.y][coordinate.x]
+
+fun getRegions(map: Array<CharArray>): Set<Set<Coordinate>> {
+    val regions = mutableMapOf<Coordinate, MutableSet<Coordinate>>()
+    val bounds = Coordinate(map[0].size, map.size)
+
+    for (row in map.indices) {
+        for (col in map[0].indices) {
+            val coordinate = Coordinate(col, row)
+            val char = map.get(coordinate)
+
+            if (coordinate !in regions) regions[coordinate] = mutableSetOf(coordinate)
+            val set = regions[coordinate]!!
+
+            val right = coordinate.move(Direction.RIGHT)
+            if (right.inBounds(bounds) && map.get(right) == char) {
+                val rightSet = regions[right] ?: mutableSetOf(right)
+
+                set.addAll(rightSet)
+                for (other in rightSet) regions[other] = set
+            }
+
+            val up = coordinate.move(Direction.UP)
+            if (up.inBounds(bounds) && map.get(up) == char) {
+                set.add(up)
+                regions[up] = set
+            }
+        }
+    }
+
+    return regions.values.toSet()
+}
+
+fun main() {
+    val map = readData(Year._2024, 12)
+        .map { it.toCharArray() }.toTypedArray()
+
+    val regions = getRegions(map)
+    val results = regions.map { Triple(it.area(), it.perimeter(), it.countSides()) }
+
+    println(results.sumOf { it.first * it.second })
+    println(results.sumOf { it.first * it.third })
+}
--- kotlin/src/main/kotlin/library/Geometry.kt
+++ kotlin/src/main/kotlin/library/Geometry.kt
@@ -80,6 +80,9 @@ data class Coordinate(val x: Int, val y: Int) {
             else Incline.NEGATIVE
         ) else if (this.y < other.y) Incline.NEGATIVE else Incline.NONE
 
+    companion object {
+        fun origin(): Coordinate = Coordinate(0, 0)
+    }
 }
 
 fun Map<Coordinate, Char>.draw(default: Char = ' '): String {
@@ -97,6 +100,81 @@ fun Map<Coordinate, Char>.draw(default: Char = ' '): String {
         result.add(string)
     }
     return result.joinToString("\n")
+}
+
+/**
+ * Assume that they are all connected to each other.
+ */
+typealias Region = Set<Coordinate>
+
+fun Region.area(): Int = this.size
+
+fun Region.perimeter(): Int =
+    this.sumOf { (it.getNeighbours() - this).size }
+
+fun Region.getBorders(): Map<Coordinate, Set<Direction>> {
+    val result = mutableMapOf<Coordinate, Set<Direction>>()
+    for (coordinate in this) {
+        val set = Direction.entries
+            .filter { coordinate.move(it) !in this }
+            .toSet()
+        if (set.isNotEmpty()) result[coordinate] = set
+    }
+    return result
+}
+
+fun Region.countSides(): Int {
+    val borders = this.getBorders()
+        .mapValues { it.value.toMutableSet() }
+        .toMutableMap()
+
+    fun remove(c: Coordinate, d: Direction) {
+        val set = borders[c]!!
+        set.remove(d)
+        if (set.isEmpty()) borders.remove(c)
+    }
+
+    fun trace(first: Coordinate, dir: Direction): Int {
+        fun back(coordinate: Coordinate, direction: Direction): Boolean =
+            coordinate == first && direction == dir
+
+        var current = Pair(first, dir)
+        var total = 0
+        outer@ do {
+            val edges = borders[current.first]!!
+            remove(current.first, current.second)
+            var nextDir = current.second.clockwise()
+
+            while (nextDir in edges || back(current.first, nextDir)) {
+                total++
+                if (back(current.first, nextDir)) break@outer
+                remove(current.first, nextDir)
+                nextDir = nextDir.clockwise()
+            }
+
+            var next = current.first.move(nextDir)
+            nextDir = nextDir.counterClockwise()
+
+            if (back(next, nextDir)) break@outer
+
+            if (next !in borders || nextDir !in borders[next]!!) {
+                next = next.move(nextDir)
+                nextDir = nextDir.counterClockwise()
+                total++
+                if (back(next, nextDir)) break@outer
+            }
+
+            current = Pair(next, nextDir)
+        } while (true)
+        return total
+    }
+
+    var total = 0
+    while (borders.isNotEmpty()) {
+        val first = borders.keys.first()
+        total += trace(first, borders[first]!!.first())
+    }
+    return total
 }
 
 data class Line(private val start: Coordinate, private val end: Coordinate) {
