177da46cf18bb6a2891c430021ee07feac8f92b9
Sára Juhošová <sarajuhosova@gmail.com>
2024-12-16 10:08:44 +0100

    Do 2024 D16P2

--- kotlin/src/main/kotlin/aoc2024/day16/Solution.kt
+++ kotlin/src/main/kotlin/aoc2024/day16/Solution.kt
@@ -33,31 +33,45 @@ fun parseMap(input: List<String>): Pair<Set<Coordinate>, Pair<Coordinate, Coordi
     return Pair(path, Pair(start!!, end!!))
 }
 
-fun Set<Coordinate>.shortest(start: Coordinate, end: Coordinate): Long {
-    val map = this.associateWith { mutableMapOf<Direction, Long>() }.toMutableMap()
-    map[start]!![START_DIRECTION] = 0L
+fun Set<Coordinate>.shortest(start: Coordinate, end: Coordinate): Pair<Set<List<Coordinate>>, Long> {
+    val map = this.associateWith { mutableMapOf<Direction, Pair<MutableSet<List<Coordinate>>, Long>>() }.toMutableMap()
+    map[start]!![START_DIRECTION] = Pair(mutableSetOf(listOf(start)), 0L)
 
     val queue = PriorityQueue<Pair<Pair<Coordinate, Direction>, Long>>(compareBy { it.second })
     queue.add((start to START_DIRECTION) to 0L)
 
     while (queue.isNotEmpty()) {
         val (heading, distance) = queue.poll()
+        val paths = map[heading.first]!![heading.second]!!.first
+
         val neighbours = heading.first.getNeighbourMap().filterValues { it in this }
         for ((direction, neighbour) in neighbours) {
             val nDistance = distance + (if (direction == heading.second) 1L else 1001L)
             val distances = map[neighbour]!!
-            if (direction !in distances || distances[direction]!! > nDistance) {
-                distances[direction] = nDistance
-                queue.add(Pair(Pair(neighbour, direction), nDistance))
+            if (direction !in distances || distances[direction]!!.second >= nDistance) {
+                val pathsFromHere = paths.map { it + listOf(neighbour) }.toMutableSet()
+
+                if (direction in distances && distances[direction]!!.second == nDistance) {
+                    distances[direction]!!.first.addAll(pathsFromHere)
+                } else {
+                    distances[direction] = Pair(pathsFromHere, nDistance)
+                    queue.add(Pair(Pair(neighbour, direction), nDistance))
+                }
             }
         }
     }
 
-    return map[end]!!.map { it.value }.min()
+    val shortest = map[end]!!.map { it.value.second }.min()
+    return Pair(
+        map[end]!!.map { it.value }.filter { it.second == shortest }.flatMap { it.first }.toSet(),
+        shortest
+    )
 }
 
 fun main() {
     val (path, positions) = parseMap(readData(Year._2024, 16))
 
-    println(path.shortest(positions.first, positions.second))
+    val (paths, distance) = path.shortest(positions.first, positions.second)
+    println(distance)
+    println(paths.flatten().toSet().size)
 }
