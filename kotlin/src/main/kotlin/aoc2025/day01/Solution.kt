c1596a0095e44bce38711e906b2da62c37f2b5aa
Sára Juhošová <sarajuhosova@gmail.com>
2025-12-01 11:27:59 +0100

    Refactor 2025 D1

--- kotlin/src/main/kotlin/aoc2025/day01/Solution.kt
+++ kotlin/src/main/kotlin/aoc2025/day01/Solution.kt
@@ -3,66 +3,58 @@ package aoc2025.day01
 import library.Year
 import library.readData
 
+const val START_POSITION = 50
 const val DIAL_SIZE = 100
 
-fun parse(command: String): (Int) -> Int {
+typealias Operation = (Int, Int) -> Int
+typealias Rotation = Pair<Operation, Int>
+
+fun modulate(position: Int): Int = ((position % DIAL_SIZE) + DIAL_SIZE) % DIAL_SIZE
+
+fun parse(command: String): Rotation {
     val moves = command.substring(1).toInt()
-    return when (command[0]) {
-        'R' -> { i -> (i + moves) % DIAL_SIZE }
-        'L' -> { i -> ((i - moves) % DIAL_SIZE + DIAL_SIZE) % DIAL_SIZE }
+    val operation: Operation = when (command[0]) {
+        'L' -> { i, j -> i - j }
+        'R' -> { i, j -> i + j }
         else -> throw IllegalArgumentException("Unknown command: $command")
     }
+    return Pair(operation, moves)
 }
 
-fun part1(commands: List<String>): Int {
-    val rotations = commands.map { parse(it) }
-
+fun part1(rotations: List<Rotation>): Int {
     var count = 0
-    var position = 50
-    for (f in rotations) {
-        position = f(position)
+    var position = START_POSITION
+    for (rotation in rotations) {
+        position = modulate(rotation.first(position, rotation.second))
         if (position == 0) count++
     }
 
     return count
 }
 
-fun part2(commands: List<String>): Int {
+fun part2(rotations: List<Rotation>): Int {
     var count = 0
-    var position = 50
+    var position = START_POSITION
 
-    for (c in commands) {
-
-        var moves = c.substring(1).toInt()
+    for (rotation in rotations) {
+        var moves = rotation.second
         count += moves / DIAL_SIZE
         moves %= DIAL_SIZE
 
-        when (c[0]) {
-            'R' -> {
-                position += moves
-                if (position > DIAL_SIZE) {
-                    count++
-                    position -= DIAL_SIZE
-                }
-            }
-            'L' -> {
-                position -= moves
-                if (position < 0) {
-                    count++
-                    position += DIAL_SIZE
-                }
-            }
-        }
-
+        position = rotation.first(position, moves)
         if (position == 0) count++
+        else if (position < 0 || position > DIAL_SIZE) {
+            count++
+            position = modulate(position)
+        }
     }
 
     return count
 }
 
 fun main() {
-    val commands = readData(Year._2025, 1)
+    val rotations = readData(Year._2025, 1).map(::parse)
 
-    println(part1(commands))
-    println(part2(commands))
+    println(part1(rotations))
+    println(part2(rotations))
 }
